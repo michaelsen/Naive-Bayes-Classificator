a3949e197048e5e3ae1e0c6f5249449e1a8f13d4
Michaelsen <michaelsen@couldyounot.com>
2014-06-08 22:12:20 -0300

    final
    finito essa b

--- index.php
+++ index.php
@@ -7,16 +7,59 @@
 
         $filename = "result.txt";
         $file = fopen($filename, 'w');
+        fwrite($file, "\n");//erases everything
 
+        $mean = array("TruePositive" => 0, "FalsePositive" => 0, "TrueNegative" => 0, "FalseNegative" => 0);
         foreach ($grouping as $key=> $group) 
         {
-            fwrite($file, "[TESTING SET = GROUP "."$key+1"."]================================\n");
+            file_put_contents($filename, "[TESTING SET = GROUP "."$key"."]================================\r\n", FILE_APPEND);
+            echo "<h3>[TESTING SET = GROUP "."$key"."]================================</h3>";
             $data = trainingPhase($grouping, $key);
             $info = testingPhase($group, $data);
-            file_put_contents($filename, $info);
-            fwrite($file, "\n\n");
+            foreach ($info as $key => $value) 
+            {
+                file_put_contents($filename, "\t"."$key"." = "."$value"."\r\n", FILE_APPEND);
+                echo "<p>"."$key"." = "."$value"."</p>";
+            }
+            file_put_contents($filename, "\r\n\r\n", FILE_APPEND);
+            foreach ($mean as $key => $value) 
+            {
+                $mean[$key] += $info[$key];
+            }
+        }
+        $filecountPositive = count($grouping[0]["positive"])*10;
+        $filecountNegative = count($grouping[0]["negative"])*10;
+
+        $mean["Accuracy"] = ($mean["TruePositive"]+$mean["TrueNegative"])/($filecountPositive + $filecountNegative);
+        $mean["Sensibility(TruePositiveRate)"] = $mean["TruePositive"] / $filecountPositive;
+        $mean["FalsePositiveRate"] = $mean["FalsePositive"] / $filecountNegative;
+        $mean["Specificity"] = $mean["TrueNegative"] / $filecountNegative;
+        $mean["Efficiency"] = ($mean["Sensibility(TruePositiveRate)"] + $mean["Specificity"])/2;
+        $mean["Precision"] = $mean["TruePositive"] / ($mean["TruePositive"] + $mean["FalsePositive"]);
+        $mean["NegativePrecision"] = $mean["TrueNegative"] / ($mean["TrueNegative"] + $mean["FalseNegative"]);
+        $mean["F-Measure"] = 2 * ($mean["Sensibility(TruePositiveRate)"] * $mean["Precision"]) / ($mean["Sensibility(TruePositiveRate)"] + $mean["Precision"]);
+
+        $mean["TruePositive"] = $mean["TruePositive"]/10;
+        $mean["FalsePositive"] = $mean["FalsePositive"]/10;
+        $mean["FalseNegative"] = $mean["FalseNegative"]/10;
+        $mean["TrueNegative"] = $mean["TrueNegative"]/10;
+
+        $deviation = standardDeviation($info, $mean);
+        
+        $mean["StandardDeviation-TruePositive"] = $deviation["TruePositive"];
+        $mean["StandardDeviation-FalsePositive"] = $deviation["FalsePositive"];
+        $mean["StandardDeviation-FalseNegative"] = $deviation["FalseNegative"];
+        $mean["StandardDeviation-TrueNegative"] = $deviation["TrueNegative"];
+
+        file_put_contents($filename, "\r\n[MEAN]================================\r\n", FILE_APPEND);
+        echo "<h1>[MEAN]================================</h1>";
+        foreach ($mean as $key => $value) 
+        {
+            file_put_contents($filename, "\t"."$key"." = "."$value"."\r\n", FILE_APPEND);
+            echo "<p>"."$key"." = "."$value"."</p>";
         }
 
+        fclose($file);
     }
 
     function loadFiles()
@@ -189,12 +232,13 @@
         $info["TrueNegative"] = $negativeEvaluation;
 
         $info["Accuracy"] = ($info["TruePositive"]+$info["TrueNegative"])/($positiveFiles + $negativeFiles);
-        $info["Sensitivity"] = $info["TruePositive"] / $positiveFiles;
+        $info["Sensibility(TruePositiveRate)"] = $info["TruePositive"] / $positiveFiles;
+        $info["FalsePositiveRate"] = $info["FalsePositive"] / $negativeFiles;
         $info["Specificity"] = $info["TrueNegative"] / $negativeFiles;
-        $info["Efficiency"] = ($info["Sensitivity"] + $info["Specificity"])/2;
+        $info["Efficiency"] = ($info["Sensibility(TruePositiveRate)"] + $info["Specificity"])/2;
         $info["Precision"] = $info["TruePositive"] / ($info["TruePositive"] + $info["FalsePositive"]);
         $info["NegativePrecision"] = $info["TrueNegative"] / ($info["TrueNegative"] + $info["FalseNegative"]);
-        $info["F-Measure"] = 2 * ($info["Sensitivity"] * $info["Precision"]) / ($info["Sensitivity"] + $info["Precision"]);
+        $info["F-Measure"] = 2 * ($info["Sensibility(TruePositiveRate)"] * $info["Precision"]) / ($info["Sensibility(TruePositiveRate)"] + $info["Precision"]);
 
         return $info;
     }
@@ -225,6 +269,25 @@
         }
 
         return ($positive >= $negative);
+    }
+
+    function standardDeviation($values, $mean)
+    {
+        $sum = array("TruePositive" => 0, "FalsePositive" => 0, "TrueNegative" => 0, "FalseNegative" => 0);
+        foreach ($values as $key => $iteration) 
+        {
+            $sum["TruePositive"] += pow(($iteration["TruePositive"] - $mean["TruePositive"]), 2);
+            $sum["FalsePositive"] += pow(($iteration["FalsePositive"] - $mean["FalsePositive"]), 2);
+            $sum["FalseNegative"] += pow(($iteration["FalseNegative"] - $mean["FalseNegative"]), 2);
+            $sum["TrueNegative"] += pow(($iteration["TrueNegative"] - $mean["TrueNegative"]), 2);            
+        }
+        foreach ($sum as $key => $value) 
+        {
+            $sum[$key] = $value/9; //n-1 pra 10-fold é 9
+            $sum[$key] = sqrt($sum[$key]);
+        }
+
+        return $sum;
     }
 
 ?>
